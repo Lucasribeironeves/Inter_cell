659c90bf6a24dd3aabfded9aeba59257b73f3509
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-07-24 16:21:59 -0300

    terminando tabelas

--- exemplo/selecionado/fones.php
+++ exemplo/selecionado/fones.php
@@ -7,16 +7,16 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Capinhas</title>
+    <title>Fones de ouvido</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
 <body>
-    <form action="capinhas.php" method="post">
+    <form action="fones.php" method="post">
         <table>
             <tr>
-                <th colspan="5">Selecione sua capinha ;D</th>
+                <th colspan="5">Selecione seu fone de ouvido ;D</th>
             </tr>
             <tr>
                 <th>Indíce</th>
@@ -27,38 +27,38 @@
             </tr>
             <tr>
                 <td>0</td>
-                <td><input type="checkbox" name="capa0" id="capa"></td>
-                <td>Samsung A32</td>
+                <td><input type="checkbox" name="fone0" id="fone"></td>
+                <td>Galaxy Buds 2 - Bluetooth</td>
                 <td><input type="number" name="qtd0" id="qtd0" min="0"></td>
-                <td>15,00</td>
+                <td>390,00</td>
             </tr>
             <tr>
                 <td>1</td>
-                <td><input type="checkbox" name="capa1" id="capa"></td>
-                <td>Samsung A33 5G</td>
+                <td><input type="checkbox" name="fone1" id="fone"></td>
+                <td>Stage Hero 3 - Bluetooth</td>
                 <td><input type="number" name="qtd1" id="qtd1" min="0"></td>
-                <td>20,00</td>
+                <td>140,00</td>
             </tr>
             <tr>
                 <td>2</td>
-                <td><input type="checkbox" name="capa2" id="capa"></td>
-                <td>Redmi Note 11 4G</td>
+                <td><input type="checkbox" name="fone2" id="fone"></td>
+                <td>Redmi Air Dots - Bluetooth</td>
                 <td><input type="number" name="qtd2" id="qtd2" min="0"></td>
-                <td>15,00</td>
+                <td>120,00</td>
             </tr>
             <tr>
                 <td>3</td>
-                <td><input type="checkbox" name="capa3" id="capa"></td>
-                <td>Moto G30</td>
+                <td><input type="checkbox" name="fone3" id="fone"></td>
+                <td>QCY T13 - Bluetooth</td>
                 <td><input type="number" name="qtd3" id="qtd3" min="0"></td>
-                <td>20,00</td>
+                <td>110,00</td>
             </tr>
             <tr>
                 <td>4</td>
-                <td><input type="checkbox" name="capa4" id="capa"></td>
-                <td>Samsung M23 5G</td>
+                <td><input type="checkbox" name="fone4" id="fone"></td>
+                <td>Galaxy Buds 2 Pro - Bluetooth</td>
                 <td><input type="number" name="qtd4" id="qtd4" min="0"></td>
-                <td>25,00</td>
+                <td>900,00</td>
             </tr>
         </table>
     </form>
--- exemplo/selecionado/peliculas.php
+++ exemplo/selecionado/peliculas.php
@@ -7,16 +7,16 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Capinhas</title>
+    <title>Películas</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
 </head>
 <body>
-    <form action="capinhas.php" method="post">
+    <form action="peliculas.php" method="post">
         <table>
             <tr>
-                <th colspan="5">Selecione sua capinha ;D</th>
+                <th colspan="5">Selecione sua película ;D</th>
             </tr>
             <tr>
                 <th>Indíce</th>
@@ -27,38 +27,38 @@
             </tr>
             <tr>
                 <td>0</td>
-                <td><input type="checkbox" name="capa0" id="capa"></td>
+                <td><input type="checkbox" name="pelicula0" id="pelicula"></td>
                 <td>Samsung A32</td>
                 <td><input type="number" name="qtd0" id="qtd0" min="0"></td>
-                <td>15,00</td>
+                <td>20,00</td>
             </tr>
             <tr>
                 <td>1</td>
-                <td><input type="checkbox" name="capa1" id="capa"></td>
+                <td><input type="checkbox" name="pelicula1" id="pelicula"></td>
                 <td>Samsung A33 5G</td>
                 <td><input type="number" name="qtd1" id="qtd1" min="0"></td>
                 <td>20,00</td>
             </tr>
             <tr>
                 <td>2</td>
-                <td><input type="checkbox" name="capa2" id="capa"></td>
+                <td><input type="checkbox" name="pelicula2" id="pelicula"></td>
                 <td>Redmi Note 11 4G</td>
                 <td><input type="number" name="qtd2" id="qtd2" min="0"></td>
-                <td>15,00</td>
+                <td>20,00</td>
             </tr>
             <tr>
                 <td>3</td>
-                <td><input type="checkbox" name="capa3" id="capa"></td>
+                <td><input type="checkbox" name="pelicula3" id="pelicula"></td>
                 <td>Moto G30</td>
                 <td><input type="number" name="qtd3" id="qtd3" min="0"></td>
                 <td>20,00</td>
             </tr>
             <tr>
                 <td>4</td>
-                <td><input type="checkbox" name="capa4" id="capa"></td>
+                <td><input type="checkbox" name="pelicula4" id="pelicula"></td>
                 <td>Samsung M23 5G</td>
                 <td><input type="number" name="qtd4" id="qtd4" min="0"></td>
-                <td>25,00</td>
+                <td>20,00</td>
             </tr>
         </table>
     </form>
