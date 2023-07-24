8604ad3f2778e34736cc97d389fed97a93352bac
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-07-24 16:10:07 -0300

    criando as capinhas

---
+++ exemplo/selecionado/capinhas.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Capinhas</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action="capinhas.php" method="post">
+        <table>
+            <tr>
+                <th colspan="5">Selecione sua capinha ;D</th>
+            </tr>
+            <tr>
+                <th>Indíce</th>
+                <th>#</th>
+                <th>Descrição</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+            </tr>
+            <tr>
+                <td>0</td>
+                <td><input type="checkbox" name="capa0" id="capa"></td>
+                <td>Samsung A32</td>
+                <td><input type="number" name="qtd0" id="qtd0" min="0"></td>
+                <td>15,00</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td><input type="checkbox" name="capa1" id="capa"></td>
+                <td>Samsung A33 5G</td>
+                <td><input type="number" name="qtd1" id="qtd1" min="0"></td>
+                <td>20,00</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td><input type="checkbox" name="capa2" id="capa"></td>
+                <td>Redmi Note 11 4G</td>
+                <td><input type="number" name="qtd2" id="qtd2" min="0"></td>
+                <td>15,00</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td><input type="checkbox" name="capa0" id="capa"></td>
+                <td>Moto G30</td>
+                <td><input type="number" name="qtd3" id="qtd3" min="0"></td>
+                <td>20,00</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td><input type="checkbox" name="capa0" id="capa"></td>
+                <td>Samsung M23 5G</td>
+                <td><input type="number" name="qtd4" id="qtd4" min="0"></td>
+                <td>25,00</td>
+            </tr>
+        </table>
+    </form>
+
+    
+</body>
