6bbfeda350027a00eae9171810a261f338a52917
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-07-24 15:53:55 -0300

    organizando

---
+++ exemplo/central/selecao_produtos.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Carrinho de compras</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action="selecao_produtos.php" method="post">
+        <table>
+            <tr>
+                <th>Bem vindo ao carrinho de compras da Inter Cell</th>
+            </tr>
+            <tr>
+                <th>Produtos</th>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Capinhas" name="capinhas"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Películas" name="peliculas"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Fones de ouvido" name="fones"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Carregadores" name="carregador"></td>
+            </tr>
+        </table><br>
+
+        <input type="submit" value="Comprar" name="compre">
+    </form>
+</body>
+</html>
