64b650fea2e1d4d0de149ab033d1d16fffba211f
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-07-31 15:48:49 -0300

    .

--- exemplo/selecionado/carregadores.php
+++ exemplo/selecionado/carregadores.php
@@ -68,35 +68,35 @@ session_start();
                 <tr>
                     <td>1</td>
                     <td><input type="checkbox" name="carregador0" id="carregador"></td>
-                    <td><input type="text" name="desc0" value="Samsung 20W" readonly></td>
+                    <td><input type="text" name="desc0" value="Samsung-USB C 20W" readonly></td>
                     <td><input type="number" name="qtd0" id="qtd0" value="0" min="0"></td>
                     <td><input type="text" name="vl0" value="59.90" readonly></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td><input type="checkbox" name="carregador1" id="carregador"></td>
-                    <td><input type="text" name="desc1" value= "Redmi 60W" readonly></td>
+                    <td><input type="text" name="desc1" value= "Redmi-USB C 60W" readonly></td>
                     <td><input type="number" name="qtd1" id="qtd1" value="0" min="0"></td>
                     <td><input type="number" name="vl1" value="34.99" readonly></td>
                 </tr>
                 <tr>
                     <td>3</td>
                     <td><input type="checkbox" name="carregador2" id="carregador"></td>
-                    <td><input type="text" name="desc2" value="Motorola 25W" readonly></td>
+                    <td><input type="text" name="desc2" value="Motorola-USB C 25W" readonly></td>
                     <td><input type="number" name="qtd2" id="qtd2" value="0" min="0"></td>
                     <td><input type="number" name="vl2" value="49.90" readonly></td>
                 </tr>
                 <tr>
                     <td>4</td>
                     <td><input type="checkbox" name="carregador3" id="carregador"></td>
-                    <td><input type="text" name="desc3" value="LG 10W" readonly></td>
+                    <td><input type="text" name="desc3" value="LG-USB 10W" readonly></td>
                     <td><input type="number" name="qtd3" id="qtd3" value="0" min="0"></td>
                     <td><input type="number" name="vl3" value="25.00" readonly></td>
                 </tr>
                 <tr>
                     <td>5</td>
                     <td><input type="checkbox" name="carregador4" id="carregador"></td>
-                    <td><input type="text" name="desc4" value="Apple 50W" readonly></td>
+                    <td><input type="text" name="desc4" value="Apple-USB C 50W" readonly></td>
                     <td><input type="number" name="qtd4" id="qtd4" value="0" min="0"></td>
                     <td><input type="number" name="vl4" value="199.90" readonly></td>
                 </tr>
